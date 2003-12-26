2363d8f5af7302924a0ce3608d971f5594710f3f
John Mark Bell <jmb@netsurf-browser.org>
2003-12-26 22:13:22 +0000

    [project @ 2003-12-26 22:13:22 by jmb] Add the constant data files referred to in the previous checkin.
    svn path=/import/netsurf/; revision=450

---
+++ riscos/constdata.c
@@ -0,0 +1,16 @@
+/*
+ * This file is part of NetSurf, http://netsurf.sourceforge.net/
+ * Licensed under the GNU General Public License,
+ *                http://www.opensource.org/licenses/gpl-license
+ * Copyright 2003 Phil Mellor <monkeyson@users.sourceforge.net>
+ * Copyright 2003 James Bursa <bursa@users.sourceforge.net>
+ * Copyright 2003 John M Bell <jmb202@ecs.soton.ac.uk>
+ */
+
+#include "netsurf/riscos/constdata.h"
+
+const char * const ABOUT_URL = "file:///%3CWimp$ScrapDir%3E/WWW/NetSurf/About";
+const char * const GESTURES_URL = "file:///%3CNetSurf$Dir%3E/Resources/gestures";
+const char * const HOME_URL = "file:///%3CNetSurf$Dir%3E/Docs/en/intro";
+const char * const HELP_URL = "file:///%3CNetSurf$Dir%3E/Docs/en/index";
+const char * const THEMES_URL = "http://netsurf.sourceforge.net/themes/";
---
+++ riscos/constdata.h
@@ -0,0 +1,19 @@
+/*
+ * This file is part of NetSurf, http://netsurf.sourceforge.net/
+ * Licensed under the GNU General Public License,
+ *                http://www.opensource.org/licenses/gpl-license
+ * Copyright 2003 Phil Mellor <monkeyson@users.sourceforge.net>
+ * Copyright 2003 James Bursa <bursa@users.sourceforge.net>
+ * Copyright 2003 John M Bell <jmb202@ecs.soton.ac.uk>
+ */
+
+#ifndef _NETSURF_RISCOS_CONSTDATA_H_
+#define _NETSURF_RISCOS_CONSTDATA_H_
+
+extern const char * const ABOUT_URL;
+extern const char * const GESTURES_URL;
+extern const char * const HOME_URL;
+extern const char * const HELP_URL;
+extern const char * const THEMES_URL;
+
+#endif
