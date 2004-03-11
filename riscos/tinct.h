7fcad102667f670018d64aeafa13716ed2e2c53a
Richard Wilson <rjw@netsurf-browser.org>
2004-03-11 23:19:27 +0000

    [project @ 2004-03-11 23:19:27 by rjw] Added Tinct_Plot and Tinct_PlotScaled definitions and modified comments to reflect the updated operation.
    svn path=/import/netsurf/; revision=608

--- riscos/tinct.h
+++ riscos/tinct.h
@@ -18,8 +18,18 @@
 #define Tinct_PlotAlpha 0x57240
 
 /*	Tinct_PlotScaledAlpha plots the RGBA sprite pointed to by R2 at the OS screen location (R3,R4)
-	scaled to (R5,R6) pixels. Flags are supplied in R7 - see the Tinct documentation for further details.
+	scaled to (R5,R6) OS units. Flags are supplied in R7 - see the Tinct documentation for further details.
 */
 #define Tinct_PlotScaledAlpha 0x57241
+
+/*	Tinct_PlotAlpha plots the RGB0 sprite pointed to by R2 at the OS screen location (R3,R4). Flags are
+	supplied in R7 - see the Tinct documentation for further details.
+*/
+#define Tinct_Plot 0x57242
+
+/*	Tinct_PlotScaledAlpha plots the RGB0 sprite pointed to by R2 at the OS screen location (R3,R4)
+	scaled to (R5,R6) OS units. Flags are supplied in R7 - see the Tinct documentation for further details.
+*/
+#define Tinct_PlotScaled 0x57243
 
 #endif
