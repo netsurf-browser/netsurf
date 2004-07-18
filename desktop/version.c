c1dc4b555e05b370fce7cf1ba30b228373d4dc8b
James Bursa <james@netsurf-browser.org>
2004-07-18 22:41:49 +0000

    [project @ 2004-07-18 22:41:49 by bursa] Reverse accidental checkin (breaks autobuilder).
    svn path=/import/netsurf/; revision=1099

--- desktop/version.c
+++ desktop/version.c
@@ -1,8 +1,2 @@
-/*
- * This file is part of NetSurf, http://netsurf.sourceforge.net/
- * Licensed under the GNU General Public License,
- *                http://www.opensource.org/licenses/gpl-license
- * Copyright 2004 Andrew Timmins <atimmins@blueyonder.co.uk>
- */
-
 const char * const netsurf_version = "Development Build";
+
