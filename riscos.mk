552c373d854837fdc3a4ef348aae9d9cde45ebe0
Richard Wilson <rjw@netsurf-browser.org>
2004-07-25 20:48:25 +0000

    [project @ 2004-07-25 20:48:25 by rjw] Removed use of libpng library
    svn path=/import/netsurf/; revision=1145

--- riscos.mk
+++ riscos.mk
@@ -8,7 +8,7 @@ LDFLAGS_RISCOS = NSLibs:lib/libxml2 NSLibs:lib/libz NSLibs:lib/libcurl \
 	NSLibs:lib/libssl NSLibs:lib/libcrypto NSLibs:lib/libares \
 	NSLibs:lib/libmng NSLibs:lib/liblcms NSLibs:lib/libjpeg OSLib:o.oslib32
 LDFLAGS_SMALL = NSLibs:lib/libxml2 NSLibs:lib/libz NSLibs:lib/libcurl \
-	NSLibs:lib/libares NSLibs:lib/libpng NSLibs:lib/libmng \
+	NSLibs:lib/libares NSLibs:lib/libmng \
 	NSLibs:lib/libjpeg OSLib:o.oslib32
 
 RUNIMAGE = !NetSurf/!RunImage
