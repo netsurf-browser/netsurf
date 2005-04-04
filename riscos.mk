57c30e59f270afc02d4bdbc0b0fbcd509302c195
Adrian Lees <adrian@aemulor.com>
2005-04-04 23:42:47 +0000

    [project @ 2005-04-04 23:42:47 by adrianl] Added throwback
    svn path=/import/netsurf/; revision=1597

--- riscos.mk
+++ riscos.mk
@@ -1,8 +1,8 @@
 CC = gcc
 CC_DEBUG = gcc
 
-PLATFORM_CFLAGS_RISCOS = -INSLibs:include -IOSLib:
-PLATFORM_CFLAGS_DEBUG = -INSLibs:include -IOSLib:
+PLATFORM_CFLAGS_RISCOS = -mthrowback -INSLibs:include -IOSLib:
+PLATFORM_CFLAGS_DEBUG = -mthrowback -INSLibs:include -IOSLib:
 
 LDFLAGS_RISCOS = NSLibs:lib/libxml2 NSLibs:lib/libz NSLibs:lib/libcurl \
 	NSLibs:lib/libssl NSLibs:lib/libcrypto NSLibs:lib/libcares \
