c39e6e7b4968ef483948f8b726695fd405488036
James Bursa <james@netsurf-browser.org>
2005-11-20 20:23:09 +0000

    [project @ 2005-11-20 20:23:09 by bursa] Add libpencil.
    svn path=/import/netsurf/; revision=1881

--- posix.mk
+++ posix.mk
@@ -8,9 +8,9 @@ PLATFORM_CFLAGS_DEBUG = -I/usr/include/libxml2 -I/riscos/src/OSLib \
 		-I/riscos/include/libjpeg -D_POSIX_C_SOURCE
 
 LDFLAGS_RISCOS = -L/home/riscos/env/lib -lxml2 -lz -lcurl -lssl -lcrypto \
-		-lcares -lmng -loslib -ljpeg -lrufl
+		-lcares -lmng -loslib -ljpeg -lrufl -lpencil
 LDFLAGS_SMALL = -L/home/riscos/env/lib -lxml2 -lz -lucurl -lcares -lmng \
-		-loslib -ljpeg -lrufl
+		-loslib -ljpeg -lrufl -lpencil
 LDFLAGS_DEBUG = -L/usr/lib -lxml2 -lz -lm -lcurl -lssl -lcrypto -ldl -lmng \
 		-ljpeg -llcms
 
