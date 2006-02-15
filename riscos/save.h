5f93f8f3f94c852ce3717ab79d43a1c7461ddf00
Adrian Lees <adrian@aemulor.com>
2006-02-15 23:10:08 +0000

    [project @ 2006-02-15 23:10:08 by adrianl] New header files
    svn path=/import/netsurf/; revision=2082

---
+++ riscos/save.h
@@ -0,0 +1,31 @@
+/*
+ * This file is part of NetSurf, http://netsurf.sourceforge.net/
+ * Licensed under the GNU General Public License,
+ *                http://www.opensource.org/licenses/gpl-license
+ * Copyright 2006 Adrian Lees <adrianl@users.sourceforge.net>
+ */
+
+/** \file
+ * File/object/selection saving (Interface).
+ */
+
+#ifndef _NETSURF_RISCOS_SAVE_H_
+#define _NETSURF_RISCOS_SAVE_H_
+
+#include "oslib/wimp.h"
+#include "netsurf/desktop/gui.h"
+
+
+wimp_w ro_gui_saveas_create(const char *template_name);
+void ro_gui_saveas_quit(void);
+void ro_gui_save_prepare(gui_save_type save_type, struct content *c);
+void ro_gui_save_start_drag(wimp_pointer *pointer);
+void ro_gui_drag_icon(int x, int y, const char *sprite);
+void ro_gui_drag_box_cancel(void);
+void ro_gui_save_drag_end(wimp_dragged *drag);
+void ro_gui_send_datasave(gui_save_type save_type, wimp_full_message_data_xfer *message, wimp_t to);
+void ro_gui_save_datasave_ack(wimp_message *message);
+bool ro_gui_save_ok(wimp_w w);
+void ro_gui_convert_save_path(char *dp, size_t len, const char *p);
+
+#endif
---
+++ riscos/textselection.h
@@ -0,0 +1,32 @@
+/*
+ * This file is part of NetSurf, http://netsurf.sourceforge.net/
+ * Licensed under the GNU General Public License,
+ *                http://www.opensource.org/licenses/gpl-license
+ * Copyright 2006 Adrian Lees <adrianl@users.sourceforge.net>
+ */
+
+/** \file
+ * Text selection import/export (interface).
+ */
+
+#ifndef _NETSURF_RISCOS_TEXTSELECTION_H_
+#define _NETSURF_RISCOS_TEXTSELECTION_H_
+
+#include "oslib/wimp.h"
+#include "netsurf/desktop/gui.h"
+
+
+void ro_gui_selection_drag_end(struct gui_window *g, wimp_dragged *drag);
+void ro_gui_selection_claim_entity(wimp_full_message_claim_entity *claim);
+void ro_gui_selection_data_request(wimp_full_message_data_request *req);
+bool ro_gui_save_clipboard(const char *path);
+
+/* drag-and-drop, receiving */
+void ro_gui_selection_dragging(wimp_message *message);
+void ro_gui_selection_drag_reset(void);
+
+/* drag-and-drop, sending */
+void ro_gui_selection_send_dragging(wimp_pointer *pointer);
+void ro_gui_selection_drag_claim(wimp_message *message);
+
+#endif
