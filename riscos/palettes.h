1bded081d6473fae533652e1ac8cf9942c45c7d6
Adrian Lees <adrian@aemulor.com>
2006-03-24 03:54:48 +0000

    [project @ 2006-03-24 03:54:47 by adrianl] Palette definitions for sprites
    svn path=/import/netsurf/; revision=2158

---
+++ riscos/palettes.h
@@ -0,0 +1,22 @@
+/*
+ * This file is part of NetSurf, http://netsurf.sourceforge.net/
+ * Licensed under the GNU General Public License,
+ *                http://www.opensource.org/licenses/gpl-license
+ * Copyright 2006 Adrian Lees <adrianl@users.sourceforge.net>
+ */
+
+/** \file
+ * Palette definitions for sprites
+ */
+
+#ifndef __NETSURF_RISCOS_PALETTES_H_
+#define __NETSURF_RISCOS_PALETTES_H_
+#include "oslib/os.h"
+
+extern const os_colour default_palette1[];
+extern const os_colour default_palette2[];
+extern const os_colour wimp_palette[];
+extern const os_colour default_palette8[];
+extern const os_colour captured_palette8[];
+
+#endif
