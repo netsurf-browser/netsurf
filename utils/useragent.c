9ef3a2cd577cb45e6d68b691f39e4ada91417bb8
Daniel Silverstone <dsilvers@netsurf-browser.org>
2007-06-10 18:13:59 +0000

    Reflow useragent.c to be 8 char indent. Also rename build_user_agent to user_agent_build_string so that all useragent.c starts user_agent_*
    svn path=/trunk/netsurf/; revision=3334

--- utils/useragent.c
+++ utils/useragent.c
@@ -23,46 +23,46 @@ static const char *core_user_agent_string = NULL;
  * user agent in HTTP requests.
  */
 static void
-build_user_agent(void)
+user_agent_build_string(void)
 {
-  struct utsname un;
-  const char *sysname = "Unknown";
-  const char *machine = "Unknown";
-  char *ua_string;
-  int len;
+        struct utsname un;
+        const char *sysname = "Unknown";
+        const char *machine = "Unknown";
+        char *ua_string;
+        int len;
 
-  if (uname(&un) == 0) {
-    sysname = un.sysname;
-    machine = un.machine;
-  }
+        if (uname(&un) == 0) {
+                sysname = un.sysname;
+                machine = un.machine;
+        }
 
-  len = snprintf(NULL, 0, NETSURF_UA_FORMAT_STRING,
+        len = snprintf(NULL, 0, NETSURF_UA_FORMAT_STRING,
+                       netsurf_version_major,
+                       netsurf_version_minor,
+                       sysname,
+                       machine);
+        ua_string = malloc(len + 1);
+        if (!ua_string) {
+                /** \todo this needs handling better */
+                return;
+        }
+        snprintf(ua_string, len + 1,
+                 NETSURF_UA_FORMAT_STRING,
                  netsurf_version_major,
                  netsurf_version_minor,
                  sysname,
                  machine);
-  ua_string = malloc(len + 1);
-  if (!ua_string) {
-    /** \todo this needs handling better */
-    return;
-  }
-  snprintf(ua_string, len + 1,
-           NETSURF_UA_FORMAT_STRING,
-           netsurf_version_major,
-           netsurf_version_minor,
-           sysname,
-           machine);
 
-  core_user_agent_string = ua_string;
+        core_user_agent_string = ua_string;
 
-  LOG(("Built user agent \"%s\"", core_user_agent_string));
+        LOG(("Built user agent \"%s\"", core_user_agent_string));
 }
 
 /* This is a function so that later we can override it trivially */
 const char *
 user_agent_string(void)
 {
-  if (core_user_agent_string == NULL)
-    build_user_agent();
-  return core_user_agent_string;
+        if (core_user_agent_string == NULL)
+                user_agent_build_string();
+        return core_user_agent_string;
 }
