43fe5333408bb2e7c7be1c12f95020809ef21d97
James Bursa <james@netsurf-browser.org>
2008-06-14 22:47:00 +0000

    Remove nonsensical disc cache section from Choices window.
    svn path=/trunk/netsurf/; revision=4331

--- riscos/configure/con_cache.c
+++ riscos/configure/con_cache.c
@@ -33,13 +33,9 @@
 #define CACHE_MEMORY_SIZE 3
 #define CACHE_MEMORY_DEC 4
 #define CACHE_MEMORY_INC 5
-#define CACHE_DURATION_SIZE 10
-#define CACHE_DURATION_DEC 11
-#define CACHE_DURATION_INC 12
-#define CACHE_MAINTAIN_BUTTON 14
-#define CACHE_DEFAULT_BUTTON 15
-#define CACHE_CANCEL_BUTTON 16
-#define CACHE_OK_BUTTON 17
+#define CACHE_DEFAULT_BUTTON 7
+#define CACHE_CANCEL_BUTTON 8
+#define CACHE_OK_BUTTON 9
 
 static bool ro_gui_options_cache_click(wimp_pointer *pointer);
 static bool ro_gui_options_cache_ok(wimp_w w);
@@ -48,13 +44,10 @@ bool ro_gui_options_cache_initialise(wimp_w w) {
 	/* set the current values */
 	ro_gui_set_icon_decimal(w, CACHE_MEMORY_SIZE,
 			(option_memory_cache_size * 10) >> 20, 1);
-	ro_gui_set_icon_decimal(w, CACHE_DURATION_SIZE, option_disc_cache_age, 0);
 
 	/* initialise all functions for a newly created window */
 	ro_gui_wimp_event_register_numeric_field(w, CACHE_MEMORY_SIZE,
 			CACHE_MEMORY_INC, CACHE_MEMORY_DEC, 0, 64, 1, 1);
-	ro_gui_wimp_event_register_numeric_field(w, CACHE_DURATION_SIZE,
-			CACHE_DURATION_INC, CACHE_DURATION_DEC, 0, 28, 1, 0);
 	ro_gui_wimp_event_register_mouse_click(w, ro_gui_options_cache_click);
 	ro_gui_wimp_event_register_cancel(w, CACHE_CANCEL_BUTTON);
 	ro_gui_wimp_event_register_ok(w, CACHE_OK_BUTTON,
@@ -71,13 +64,6 @@ bool ro_gui_options_cache_click(wimp_pointer *pointer) {
 			/* set the default values */
 			ro_gui_set_icon_decimal(pointer->w, CACHE_MEMORY_SIZE,
 					20, 1);
-			ro_gui_set_icon_decimal(pointer->w, CACHE_DURATION_SIZE,
-					28, 0);
-			return true;
-		case CACHE_MAINTAIN_BUTTON:
-			xhourglass_on();
-			filename_flush();
-			xhourglass_off();
 			return true;
 	}
 	return false;
@@ -86,7 +72,6 @@ bool ro_gui_options_cache_click(wimp_pointer *pointer) {
 bool ro_gui_options_cache_ok(wimp_w w) {
 	option_memory_cache_size = (((ro_gui_get_icon_decimal(w,
 			CACHE_MEMORY_SIZE, 1) + 1) << 20) - 1) / 10;
-	option_disc_cache_age = ro_gui_get_icon_decimal(w, CACHE_DURATION_SIZE, 0);
 
 	ro_gui_save_options();
   	return true;
