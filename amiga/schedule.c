d1c876e03ce1df47dab9c77fa5c5b7192670267d
Chris Young <chris@unsatisfactorysoftware.co.uk>
2008-08-03 16:10:10 +0000

    Fully implemented schedule, schedule_run and schedule_remove.
    svn path=/trunk/netsurf/; revision=4875

--- amiga/schedule.c
+++ amiga/schedule.c
@@ -17,16 +17,119 @@
  */
 
 #include "desktop/browser.h"
+#include "amiga/object.h"
+#include "amiga/schedule.h"
+#include <proto/exec.h>
+#include <proto/timer.h>
+
+/**
+ * Schedule a callback.
+ *
+ * \param  t         interval before the callback should be made / cs
+ * \param  callback  callback function
+ * \param  p         user parameter, passed to callback function
+ *
+ * The callback function will be called as soon as possible after t cs have
+ * passed.
+ */
 
 void schedule(int t, void (*callback)(void *p), void *p)
 {
-	printf("adding callback %lx\n",callback);
+	struct nsObject *obj;
+	struct nscallback *nscb;
+	struct timeval tv;
+
+	printf("adding callback %lx,%ld at %d cs\n",callback,p,t);
+
+	obj = AddObject(schedule_list,AMINS_CALLBACK);
+	obj->objstruct_size = sizeof(struct nscallback);
+	obj->objstruct = AllocVec(obj->objstruct_size,MEMF_CLEAR);
+
+	nscb = (struct nscallback *)obj->objstruct;
+
+	nscb->tv.tv_sec = 0;
+	nscb->tv.tv_micro = t*10000;
+	GetSysTime(&tv);
+	AddTime(&nscb->tv,&tv);
+
+	nscb->callback = callback;
+	nscb->p = p;
 }
+
+/**
+ * Unschedule a callback.
+ *
+ * \param  callback  callback function
+ * \param  p         user parameter, passed to callback function
+ *
+ * All scheduled callbacks matching both callback and p are removed.
+ */
 
 void schedule_remove(void (*callback)(void *p), void *p)
 {
+	printf("remove schedule %lx,%lx\n",callback,p);
+
+	struct nsObject *node;
+	struct nsObject *nnode;
+	struct nscallback *nscb;
+
+	if(IsMinListEmpty(schedule_list)) return;
+
+	node = (struct nsObject *)schedule_list->mlh_Head;
+
+	while(nnode=(struct nsObject *)(node->dtz_Node.mln_Succ))
+	{
+		printf("rem %lx next %lx node objtype %ld\n",node,nnode,node->Type);
+
+		nscb = node->objstruct;
+
+		if((nscb->callback == callback) && (nscb->p == p))
+		{
+			DelObject(node);
+		}
+
+		node=nnode;
+	}
 }
+
+/**
+ * Process events up to current time.
+ */
 
 void schedule_run(void)
 {
+	struct nsObject *node;
+	struct nsObject *nnode;
+	struct nscallback *nscb;
+	void (*callback)(void *p);
+	struct timeval tv;
+
+	if(IsMinListEmpty(schedule_list)) return;
+
+	GetSysTime(&tv);
+
+	node = (struct nsObject *)schedule_list->mlh_Head;
+
+	while(nnode=(struct nsObject *)(node->dtz_Node.mln_Succ))
+	{
+		printf("run %lx next %lx node objtype %ld\n",node,nnode,node->Type);
+
+		if((node->Type == AMINS_CALLBACK) && (node->objstruct))
+		{
+			nscb = node->objstruct;
+
+			printf("%ld %ld <now>\n%ld %ld\n",tv.tv_secs,tv.tv_micro,nscb->tv.tv_secs,nscb->tv.tv_micro);
+
+			if(CmpTime(&tv,&nscb->tv) <= 0)
+			{
+				printf("running callback\n");
+
+				callback = nscb->callback;
+				callback(nscb->p);
+				DelObject(node);
+			}
+		}
+
+		node=nnode;
+	}
 }
