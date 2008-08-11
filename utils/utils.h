00a1a0d35ca986d2ee60edf4d93f2bd4f670d6b5
John Mark Bell <jmb@netsurf-browser.org>
2008-08-11 08:57:05 +0000

    Squash warnings on RISC OS
    svn path=/trunk/netsurf/; revision=5023

--- utils/utils.h
+++ utils/utils.h
@@ -75,7 +75,7 @@ void regcomp_wrapper(regex_t *preg, const char *regex, int cflags);
 void unicode_transliterate(unsigned int c, char **r);
 char *human_friendly_bytesize(unsigned long bytesize);
 const char *rfc1123_date(time_t t);
-#if !(defined(_GNU_SOURCE) || defined(__NetBSD__))
+#if !(defined(_GNU_SOURCE) || defined(__NetBSD__) || defined(riscos))
 char *strcasestr(const char *haystack, const char *needle);
 #endif
 unsigned int wallclock(void);
