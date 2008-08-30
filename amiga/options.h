eb006497957756b31e0b9ba06af2a5c23d2554aa
Chris Young <chris@unsatisfactorysoftware.co.uk>
2008-08-30 21:41:35 +0000

    Option to stop iframes popping up in new windows
    svn path=/trunk/netsurf/; revision=5223

--- amiga/options.h
+++ amiga/options.h
@@ -25,18 +25,24 @@ extern char *option_url_file;
 extern char *option_hotlist_file;
 extern bool option_use_wb;
 extern int option_modeid;
+extern char *option_toolbar_images;
+extern bool option_no_iframes;
 
 #define EXTRA_OPTION_DEFINE \
 bool option_verbose_log = false; \
 char *option_url_file = 0; \
 char *option_hotlist_file = 0; \
 bool option_use_wb = false; \
-int option_modeid = 0;
+int option_modeid = 0; \
+char *option_toolbar_images = 0; \
+bool option_no_iframes = false; \
 
 #define EXTRA_OPTION_TABLE \
 { "verbose_log",	OPTION_BOOL,	&option_verbose_log}, \
 { "url_file",		OPTION_STRING,	&option_url_file }, \
 { "hotlist_file",		OPTION_STRING,	&option_hotlist_file }, \
 { "use_workbench",	OPTION_BOOL,	&option_use_wb}, \
-{ "screen_modeid",	OPTION_INTEGER,	&option_modeid},
+{ "screen_modeid",	OPTION_INTEGER,	&option_modeid}, \
+{ "toolbar_images",		OPTION_STRING,	&option_toolbar_images }, \
+{ "no_iframes",	OPTION_BOOL,	&option_no_iframes},
 #endif
