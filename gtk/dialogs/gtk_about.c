a0273d8c7f9f765e2c9dfdbab9b34e798d111dc4
Rob Kendrick <rjek@netsurf-browser.org>
2008-10-09 21:17:31 +0000

    Fix warning in gtk_about.c, reformat
    svn path=/trunk/netsurf/; revision=5516

--- gtk/dialogs/gtk_about.c
+++ gtk/dialogs/gtk_about.c
@@ -22,17 +22,27 @@
 
 GtkAboutDialog* about_dialog;
 
-static const gchar *authors[] = {"John-Mark Bell", "James Bursa", "Michael Drake",
+static const gchar *authors[] = {
+		"John-Mark Bell", "James Bursa", "Michael Drake",
 		"Rob Kendrick", "Adrian Lees", "Vincent Sanders", "Daniel Silverstone",
 		"Richard Wilson", "\nContributors:", "Kevin Bagust", "Stefaan Claes",
 		"Matthew Hambley", "Rob Jackson", "Jeffrey Lee", "Phil Mellor",
 		"Philip Pemberton", "Darren Salt", "Andrew Timmins", "John Tytgat",
-		"Chris Williams", "\nGoogle Summer of Code Contributors:", "Adam Blokus",
-		"Sean Fox", "Michael Lester", "Andrew Sidwell", NULL};
+		"Chris Williams", "\nGoogle Summer of Code Contributors:",
+		"Adam Blokus", "Sean Fox", "Michael Lester", "Andrew Sidwell", NULL
+}
+;
 static const gchar *translators = "Sebastian Barthel \nBruno D'Arcangeli \nGerard van Katwijk \nJérôme Mathevet \nSimon Voortman.";
-static const gchar *artists[] = {"Michael Drake", "\nContributors:",
-		"Andrew Duffell", "John Duffell", "Richard Hallas", "Phil Mellor", NULL};
-static const gchar *documenters[] = {"John-Mark Bell", "James Bursa", "Michael Drake", "Richard Wilson", "\nContributors:", "James Shaw", NULL};
+static const gchar *artists[] = {
+		"Michael Drake", "\nContributors:",
+		"Andrew Duffell", "John Duffell", "Richard Hallas", "Phil Mellor",
+		NULL
+}
+;
+static const gchar *documenters[] = {
+		"John-Mark Bell", "James Bursa", "Michael Drake", "Richard Wilson",
+		"\nContributors:", "James Shaw", NULL
+};
 
 static const gchar *name = "NetSurf";
 static const gchar *description = "Small as a mouse, fast as a cheetah, and available for free.\nNetSurf is a web browser for RISC OS and UNIX-like platforms.";
@@ -40,15 +50,17 @@ static const gchar *url = "http://www.netsurf-browser.org/";
 static const gchar *url_label = "NetSurf Website";
 static const gchar *copyright = "Copyright © 2003 - 2008 The NetSurf Developers";
 
-static const gchar* licence = "licence";
-
-static void launch_url (GtkAboutDialog *about_dialog, const gchar *url, gpointer data){
-      struct browser_window *bw = data;
-	 browser_window_go(bw, url, 0, true);
+static void launch_url (GtkAboutDialog *about_dialog, const gchar *url, gpointer data)
+{
+	struct browser_window *bw = data;
+	browser_window_go(bw, url, 0, true);
  }
 
-void nsgtk_about_dialog_init(GtkWindow *parent, struct browser_window *bw, const char *version) {
-	g_file_get_contents(g_strconcat(res_dir_location, licence, NULL), &licence, NULL, NULL);
+void nsgtk_about_dialog_init(GtkWindow *parent, struct browser_window *bw, const char *version)
+{
+	gchar *licence_text;
+	
+	g_file_get_contents(g_strconcat(res_dir_location, "licence", NULL), &licence_text, NULL, NULL);
 	gtk_about_dialog_set_url_hook (launch_url, (gpointer) bw, NULL);
 
 	gtk_show_about_dialog(parent, "artists", artists, "authors", authors,
