f5587d22a418904989b2fc52030770e03e41a06c
John Mark Bell <jmb@netsurf-browser.org>
2009-03-29 12:49:17 +0000

    Squash warning
    svn path=/trunk/netsurf/; revision=6986

--- gtk/gtk_history.c
+++ gtk/gtk_history.c
@@ -16,8 +16,11 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <string.h>
+
 #include <gtk/gtk.h>
 #include <glade/glade.h>
+
 #include "utils/log.h"
 #include "utils/utils.h"
 #include "utils/url.h"
