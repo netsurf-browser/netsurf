d66ffb6e85dd33deb85c82c318fc90e97be1c366
John Mark Bell <jmb@netsurf-browser.org>
2009-05-06 23:52:12 +0000

    It's probably about time we stopped claiming to be 2.0
    svn path=/trunk/netsurf/; revision=7411

--- desktop/version.c
+++ desktop/version.c
@@ -1,3 +1,3 @@
-const char * const netsurf_version = "2.0 (Development)";
-const int netsurf_version_major = 2;
+const char * const netsurf_version = "3.0 (Development)";
+const int netsurf_version_major = 3;
 const int netsurf_version_minor = 0;
