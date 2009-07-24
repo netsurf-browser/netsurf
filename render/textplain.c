cc662408b79e0a82f707c498721c47bdd553301a
John Mark Bell <jmb@netsurf-browser.org>
2009-07-24 00:17:43 +0000

    Fix text/plain lineheight calculation
    svn path=/trunk/netsurf/; revision=8763

--- render/textplain.c
+++ render/textplain.c
@@ -736,6 +736,7 @@ float textplain_line_height(void)
 	 * Then use a constant line height of 1.2 x font size.
 	 */
 	return FIXTOFLT(FDIVI((FMUL(FLTTOFIX(1.2), 
-			FMULI(nscss_screen_dpi, textplain_style.size))), 72));
+			FMULI(nscss_screen_dpi, 
+			(textplain_style.size / FONT_SIZE_SCALE)))), 72));
 }
 
