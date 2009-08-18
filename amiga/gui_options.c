dc6e7f4627bd775c49cd02bb926d2f73509d116f
Chris Young <chris@unsatisfactorysoftware.co.uk>
2009-08-18 15:38:09 +0000

    Add missing code to apply the fetcher settings Make some stuff private to gui_options.c
    svn path=/trunk/netsurf/; revision=9349

--- amiga/gui_options.c
+++ amiga/gui_options.c
@@ -22,6 +22,8 @@
 #include <proto/intuition.h>
 #include <proto/utility.h>
 #include <libraries/gadtools.h>
+#include <exec/types.h>
+#include <intuition/classusr.h>
 
 #include "amiga/object.h"
 #include "amiga/gui.h"
@@ -57,6 +59,69 @@
 #include <gadgets/getfont.h>
 #include <reaction/reaction.h>
 #include <reaction/reaction_macros.h>
+
+enum
+{
+	GID_OPTS_MAIN=0,
+	GID_OPTS_HOMEPAGE,
+	GID_OPTS_HOMEPAGE_DEFAULT,
+	GID_OPTS_HOMEPAGE_CURRENT,
+	GID_OPTS_HIDEADS,
+	GID_OPTS_CONTENTLANG,
+	GID_OPTS_FROMLOCALE,
+	GID_OPTS_HISTORY,
+	GID_OPTS_REFERRAL,
+	GID_OPTS_FASTSCROLL,
+	GID_OPTS_SCREEN,
+	GID_OPTS_SCREENMODE,
+	GID_OPTS_SCREENNAME,
+	GID_OPTS_THEME,
+	GID_OPTS_PTRTRUE,
+	GID_OPTS_PTROS,
+	GID_OPTS_PROXY,
+	GID_OPTS_PROXY_HOST,
+	GID_OPTS_PROXY_PORT,
+	GID_OPTS_PROXY_USER,
+	GID_OPTS_PROXY_PASS,
+	GID_OPTS_FETCHMAX,
+	GID_OPTS_FETCHHOST,
+	GID_OPTS_FETCHCACHE,
+	GID_OPTS_NATIVEBM,
+	GID_OPTS_SCALEQ,
+	GID_OPTS_ANIMSPEED,
+	GID_OPTS_ANIMDISABLE,
+	GID_OPTS_FONT_SANS,
+	GID_OPTS_FONT_SERIF,
+	GID_OPTS_FONT_MONO,
+	GID_OPTS_FONT_CURSIVE,
+	GID_OPTS_FONT_FANTASY,
+	GID_OPTS_FONT_DEFAULT,
+	GID_OPTS_FONT_SIZE,
+	GID_OPTS_FONT_MINSIZE,
+	GID_OPTS_CACHE_MEM,
+	GID_OPTS_CACHE_DISC,
+	GID_OPTS_OVERWRITE,
+	GID_OPTS_DLDIR,
+	GID_OPTS_TAB_ACTIVE,
+	GID_OPTS_TAB_2,
+	GID_OPTS_CLIPBOARD,
+	GID_OPTS_CMENU_ENABLE,
+	GID_OPTS_CMENU_STICKY,
+	GID_OPTS_MARGIN_TOP,
+	GID_OPTS_MARGIN_LEFT,
+	GID_OPTS_MARGIN_BOTTOM,
+	GID_OPTS_MARGIN_RIGHT,
+	GID_OPTS_EXPORT_SCALE,
+	GID_OPTS_EXPORT_NOIMAGES,
+	GID_OPTS_EXPORT_NOBKG,
+	GID_OPTS_EXPORT_LOOSEN,
+	GID_OPTS_EXPORT_COMPRESS,
+	GID_OPTS_EXPORT_PASSWORD,
+	GID_OPTS_SAVE,
+	GID_OPTS_USE,
+	GID_OPTS_CANCEL,
+	GID_OPTS_LAST
+};
 
 enum
 {
@@ -104,6 +169,14 @@ enum
 #define OPTS_MAX_SCREEN 4
 #define OPTS_MAX_PROXY 5
 #define OPTS_MAX_NATIVEBM 3
+
+struct ami_gui_opts_window {
+	struct Window *win;
+	Object *objects[OID_LAST];
+	struct Gadget *gadgets[GID_OPTS_LAST];
+	struct nsObject *node;
+	ULONG pad[6];
+};
 
 static struct ami_gui_opts_window *gow = NULL;
 
@@ -1255,6 +1328,10 @@ void ami_gui_opts_use(void)
 	GetAttr(STRINGA_TextVal,gow->gadgets[GID_OPTS_PROXY_PASS],(ULONG *)&data);
 	if(option_http_proxy_auth_pass) free(option_http_proxy_auth_pass);
 	option_http_proxy_auth_pass = (char *)strdup((char *)data);
+
+	GetAttr(INTEGER_Number,gow->gadgets[GID_OPTS_FETCHMAX],(ULONG *)&option_max_fetchers);
+	GetAttr(INTEGER_Number,gow->gadgets[GID_OPTS_FETCHHOST],(ULONG *)&option_max_fetchers_per_host);
+	GetAttr(INTEGER_Number,gow->gadgets[GID_OPTS_FETCHCACHE],(ULONG *)&option_max_cached_fetch_handles);
 
 	GetAttr(CHOOSER_Selected,gow->gadgets[GID_OPTS_NATIVEBM],(ULONG *)&option_cache_bitmaps);
 
