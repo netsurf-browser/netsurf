7b6ca89c57ee0bc868c44b9aa3c4edbe1b46c308
François Revel <mmu_man@netsurf-browser.org>
2010-01-02 02:51:06 +0000

    Actually fake stdbool inclusion here as well.
    svn path=/trunk/netsurf/; revision=9776

--- beos/beos_search.cpp
+++ beos/beos_search.cpp
@@ -16,7 +16,8 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <stdbool.h>
+#define __STDBOOL_H__	1
+//#include <stdbool.h>
 #include <string.h>
 
 extern "C" {
