4c17317388b776e490900732fdf5f31dfeaf0e84
François Revel <mmu_man@netsurf-browser.org>
2010-08-03 21:24:55 +0000

    Move the resource id enum to its own header. Add a ersizing handle pic for the replicant.
    svn path=/trunk/netsurf/; revision=10681

---
+++ beos/beos_res.h
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2008 François Revol <mmu_man@users.sourceforge.net>
+ *
+ * This file is part of NetSurf, http://www.netsurf-browser.org/
+ *
+ * NetSurf is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * NetSurf is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+enum {
+	R_ResizeHandle = 1000,
+	R_ResBackNavActive = 1042,
+	R_ResBackNavInactive = 1043,
+	R_ResForwNavActive = 1044,
+	R_ResForwNavInactive = 1045,
+	R_ResUpNavActive = 1046,
+	R_ResUpNavInactive = 1047,
+	R_ResBackNavActiveSel = 1048,
+	R_ResForwNavActiveSel = 1049,
+	R_ResUpNavActiveSel = 1050
+};
