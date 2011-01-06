7f62710772aaa801647b7e28668b1627f006d07c
John Mark Bell <jmb@netsurf-browser.org>
2011-01-06 23:56:03 +0000

    Include os3support header
    svn path=/trunk/netsurf/; revision=11236

--- amiga/bitmap.c
+++ amiga/bitmap.c
@@ -16,6 +16,8 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include "amiga/os3support.h"
+
 #include "assert.h"
 #include "amiga/bitmap.h"
 #include <proto/exec.h>
