ebc216434fd5e090df3493e4eb444efb330ee8e4
François Revel <mmu_man@netsurf-browser.org>
2011-01-19 01:07:56 +0000

    Case insensitive filesystems should just be banned from Earth and even the Milky Way.
    svn path=/trunk/netsurf/; revision=11379

--- cocoa/NetsurfApp.m
+++ cocoa/NetsurfApp.m
@@ -16,7 +16,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#import "NetSurfApp.h"
+#import "NetsurfApp.h"
 
 #import "desktop/gui.h"
 #include "content/urldb.h"
