cb99af0b018732125effba5d13bddd562889e9b0
Daniel Silverstone <dsilvers@netsurf-browser.org>
2012-03-24 22:28:49 +0000

    Include render/box.h
    svn path=/trunk/netsurf/; revision=13638

--- desktop/browser.c
+++ desktop/browser.c
@@ -59,6 +59,7 @@
 #include "render/form.h"
 #include "render/textplain.h"
 #include "render/html.h"
+#include "render/box.h"
 #include "utils/log.h"
 #include "utils/messages.h"
 #include "utils/nsurl.h"
