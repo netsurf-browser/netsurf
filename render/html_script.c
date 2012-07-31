d763e57d30dc8c68309c5b35f556531cb8346da5
Vincent Sanders <vince@netsurf-browser.org>
2012-07-31 12:49:41 +0100

    fix dom parser error return check

--- render/html_script.c
+++ render/html_script.c
@@ -483,7 +483,7 @@ exec_src_script(html_content *c,
 
 		switch (script_type) {
 		case HTML_SCRIPT_SYNC:
-			ret =  DOM_HUBBUB_PAUSED;
+			ret =  DOM_HUBBUB_HUBBUB_ERR | HUBBUB_PAUSED;
 
 		case HTML_SCRIPT_ASYNC:
 			break;
