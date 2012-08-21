bc6087f9b60496c30c1dad488a13fd35994a69c0
Vincent Sanders <vince@netsurf-browser.org>
2012-08-21 16:43:22 +0100

    remove bogus posix define and workround missing PATH_MAX directly

--- atari/osspec.c
+++ atari/osspec.c
@@ -1,4 +1,20 @@
-#define __USE_POSIX
+/*
+ * Copyright 2010 <ole@monochrom.net>
+ *
+ * This file is part of NetSurf, http://www.netsurf-browser.org/
+ *
+ * NetSurf is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * NetSurf is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include <sys/types.h>
 #include <limits.h>
@@ -13,6 +29,10 @@
 
 #include "utils/log.h"
 #include "atari/osspec.h"
+
+#ifndef PATH_MAX
+#define PATH_MAX 1024
+#endif
 
 NS_ATARI_SYSINFO atari_sysinfo;
 
