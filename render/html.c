773cc5b562131a3a0c2399861f85d01bbe768f5b
Michael Drake <tlsa@netsurf-browser.org>
2013-03-18 15:01:56 +0000

    Stop looking on first modified sheet found.

--- render/html.c
+++ render/html.c
@@ -1135,8 +1135,10 @@ bool html_can_begin_conversion(html_content *htmlc)
 	bool got_modified_stylesheet = false;
 
 	for (i = 0; i != htmlc->stylesheet_count; i++) {
-		if (htmlc->stylesheets[i].modified)
+		if (htmlc->stylesheets[i].modified) {
 			got_modified_stylesheet = true;
+			break;
+		}
 	}
 
 	if (htmlc->base.active != 0 || got_modified_stylesheet)
