ec7b4a9ff6b2f06c95bfd890b1dc1ee6e6a1f893
Vincent Sanders <vince@netsurf-browser.org>
2013-04-19 21:24:25 +0100

    set correct version number and date

--- desktop/version.c
+++ desktop/version.c
@@ -1,10 +1,5 @@
 #include "utils/testament.h"
 
-const char * const netsurf_version = "3.0 (Dev"
-#if defined(CI_BUILD)
-	" CI #" CI_BUILD
-#endif
-	")"
-	;
+const char * const netsurf_version = "3.0 (20th April 2013)";
 const int netsurf_version_major = 3;
 const int netsurf_version_minor = 0;
