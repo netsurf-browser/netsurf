88a99d9722f6c61cf8a338002d7a38622a1168ef
Michael Drake <tlsa@netsurf-browser.org>
2014-04-02 21:56:34 +0100

    Fix logic error found by cppcheck.

--- riscos/toolbar.c
+++ riscos/toolbar.c
@@ -1579,7 +1579,7 @@ bool ro_toolbar_set_button_order(struct toolbar *toolbar, char order[])
 void ro_toolbar_set_button_shaded_state(struct toolbar *toolbar,
 		button_bar_action action, bool shaded)
 {
-	if (toolbar == NULL && toolbar->buttons == NULL)
+	if (toolbar == NULL || toolbar->buttons == NULL)
 		return;
 
 	ro_gui_button_bar_shade_button(toolbar->buttons, action, shaded);
