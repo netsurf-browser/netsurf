0a2082d77792087f8c4afff5b73f5efdfdd1b2ba
Vincent Sanders <vince@kyllikki.org>
2014-05-17 22:26:34 +0100

    ensure all vmkpath implementations check their parameters

--- riscos/gui.c
+++ riscos/gui.c
@@ -2334,6 +2334,10 @@ static nserror riscos_mkpath(char **str, size_t *size, size_t nelm, va_list ap)
 	 */
 	for (elm_idx = 0; elm_idx < nelm; elm_idx++) {
 		elm[elm_idx] = va_arg(ap, const char *);
+		/* check the argument is not NULL */
+		if (elm[elm_idx] == NULL) {
+			return NSERROR_BAD_PARAMETER;
+		}
 		elm_len[elm_idx] = strlen(elm[elm_idx]);
 		fname_len += elm_len[elm_idx];
 	}
