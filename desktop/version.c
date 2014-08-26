2d2c6b67cf44c9eef852f0cec13217eb1ad9dcab
Vincent Sanders <vince@kyllikki.org>
2014-08-26 07:57:21 -0700

    Update version for release

--- desktop/version.c
+++ desktop/version.c
@@ -1,10 +1,5 @@
 #include "testament.h"
 
-const char * const netsurf_version = "3.2 (Dev"
-#if defined(CI_BUILD)
-	" CI #" CI_BUILD
-#endif
-	")"
-	;
+const char * const netsurf_version = "3.2 (26th August 2014)";
 const int netsurf_version_major = 3;
 const int netsurf_version_minor = 2;
