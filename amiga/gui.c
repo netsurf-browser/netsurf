9e6b0fb8b07cd3147b0a2727d3644860cf7c8c7e
Chris Young <chris@unsatisfactorysoftware.co.uk>
2014-09-07 14:09:11 +0100

    Adjust min stack to 96K following evaluation.

--- amiga/gui.c
+++ amiga/gui.c
@@ -181,7 +181,7 @@ static char *current_user;
 static char *current_user_dir;
 static char *current_user_faviconcache;
 
-static const __attribute__((used)) char *stack_cookie = "\0$STACK:65536\0";
+static const __attribute__((used)) char *stack_cookie = "\0$STACK:98304\0";
 
 const char * const versvn;
 const char * const verdate;
