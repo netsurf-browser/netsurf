d6a0f3fef041da362131c7045c007e8e73b21ad4
Daniel Silverstone <dsilvers@digital-scurf.org>
2015-08-09 12:28:44 +0100

    Fix copyright header on dukky.h

--- javascript/dukky.h
+++ javascript/dukky.h
@@ -1,4 +1,25 @@
-/* DO NOT USE, DODGY BIT FOR VINCE */
+/*
+ * Copyright 2012 Vincent Sanders <vince@netsurf-browser.org>
+ * Copyright 2015 All of us.
+ *
+ * This file is part of NetSurf, http://www.netsurf-browser.org/
+ *
+ * NetSurf is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * NetSurf is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/** \file
+ * Duktapeish implementation of javascript engine functions, prototypes.
+ */
 
 #ifndef DUKKY_H
 #define DUKKY_H
