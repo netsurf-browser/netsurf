e38a1f17f204174f2160db62f5f9d901ffc3bc48
Chris Young <chris@unsatisfactorysoftware.co.uk>
2016-01-23 15:15:35 +0000

    friend_bitmap should default to false as it causes problems

--- amiga/options.h
+++ amiga/options.h
@@ -89,6 +89,6 @@ NSOPTION_INTEGER(monitor_aspect_y, 0)
 NSOPTION_BOOL(accept_lang_locale, true)
 /* Options relevant for OS3 only */
 #ifndef __amigaos4__
-NSOPTION_BOOL(friend_bitmap, true)
+NSOPTION_BOOL(friend_bitmap, false)
 #endif
 
