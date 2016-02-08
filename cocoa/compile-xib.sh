a29969888ac028b198b717165a904c9410dbf0ed
Vincent Sanders <vince@kyllikki.org>
2016-02-08 21:19:15 +0000

    Update compilation to cope with modern SDK locations

--- cocoa/compile-xib.sh
+++ cocoa/compile-xib.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 # call: compile-xib.sh [xib file] [language] [(optional output nib file)]
-DIR=`dirname "$2"`
-XIB=`basename -s .xib "$2"`
+DIR=`dirname "$1"`
+XIB=`basename -s .xib "$1"`
 
-STRINGS_FILE="$DIR/$3.lproj/$XIB.xib.strings"
+STRINGS_FILE="$DIR/$2.lproj/$XIB.xib.strings"
 TRANSLATE=""
 if [ -f $STRINGS_FILE ] 
 then
 	TRANSLATE="--strings-file $STRINGS_FILE"
 fi
 
-OUTPUT="$3.$XIB.nib"
+OUTPUT="$2.$XIB.nib"
 
-if [ "x$4" != "x" ]
+if [ "x$3" != "x" ]
 then
-	OUTPUT="$4"
+	OUTPUT="$3"
 fi
 
-exec $1/usr/bin/ibtool $TRANSLATE --compile $OUTPUT $2
+exec /usr/bin/ibtool $TRANSLATE --compile $OUTPUT $1
