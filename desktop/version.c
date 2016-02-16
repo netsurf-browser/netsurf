334873cb801bc716f1614998f064bdbe42e904d8
Vincent Sanders <vince@kyllikki.org>
2016-02-16 13:30:38 +0000

    Update version for 3.4 release

--- desktop/version.c
+++ desktop/version.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 Vincent Sanders <vince@netsurf-browser.org>
+ * Copyright 2016 Vincent Sanders <vince@netsurf-browser.org>
  *
  * This file is part of NetSurf, http://www.netsurf-browser.org/
  *
@@ -20,11 +20,6 @@
 
 #include "desktop/version.h"
 
-const char * const netsurf_version = "3.4 (Dev"
-#if defined(CI_BUILD)
-	" CI #" CI_BUILD
-#endif
-	")"
-	;
+const char * const netsurf_version = "3.4 (16th Feb 2016)";
 const int netsurf_version_major = 3;
 const int netsurf_version_minor = 4;
