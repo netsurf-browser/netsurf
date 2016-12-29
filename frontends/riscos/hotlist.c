7b7b1507d01b3e078790150d6b5f59ebaf0b45c3
Michael Drake <tlsa@netsurf-browser.org>
2016-12-29 12:21:03 +0000

    Revert "RISC OS: Ensure core's hotlist_init is called on startup."
    This reverts commit 743637a8d2105df6ffc464a3cb4753cdc8c1401f.

--- frontends/riscos/hotlist.c
+++ frontends/riscos/hotlist.c
@@ -552,25 +552,24 @@ nserror ro_gui_hotlist_present(void)
 			     error->errmess);
 	}
 
-	LOG("Presenting hotlist");
-	ro_gui_dialog_open_top(hotlist_window->core.wh,
-			hotlist_window->core.toolbar,
-			600, 800);
+	res = ro_hotlist_init();
+	if (res == NSERROR_OK) {
+		LOG("Presenting");
+		ro_gui_dialog_open_top(hotlist_window->core.wh,
+				       hotlist_window->core.toolbar,
+				       600, 800);
+	} else {
+		LOG("Failed presenting code %d", res);
+	}
 
-	return NSERROR_OK;
+	return res;
 }
 
 
 /* exported interface documented in riscos/hotlist.h */
 void ro_gui_hotlist_initialise(void)
 {
-	nserror res;
 	dialog_hotlist_template = ro_gui_dialog_load_template("tree");
-
-	res = ro_hotlist_init();
-	if (res != NSERROR_OK) {
-		LOG("Failed initialising hotlist %d", res);
-	}
 }
 
 
