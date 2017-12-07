30ee1f630a09a718a6fb603f69f257e0ae244b8c
Vincent Sanders <vince@kyllikki.org>
2017-12-07 07:56:50 +0000

    Improve cross compile tool building and error reporting
    This improves error reporting when no compiler is available. Improves
    handling of compiling of build machine tools (nsgenbind) in a cross
    compile environment.

--- docs/env.sh
+++ docs/env.sh
@@ -1,165 +1,28 @@
 #!/bin/sh
 #
-# NetSurf Library, tool and browser support script
+# NetSurf Library, tool and browser development support script
 #
-# Usage: source env.sh
-# TARGET_ABI / HOST sets the target for library builds
-# TARGET_WORKSPACE is the workspace directory to keep the sandboxes
+# Copyright 2013-2017 Vincent Sanders <vince@netsurf-browser.org>
+# Released under the MIT Licence
 #
 # This script allows NetSurf and its libraries to be built without
 #   requiring installation into a system.
 #
-# Copyright 2013 Vincent Sanders <vince@netsurf-browser.org>
-# Released under the MIT Licence
-
-# find which command used to find everything else on path
-if [ -x /usr/bin/which ]; then
-    WHICH_CMD=/usr/bin/which
-else
-    WHICH_CMD=/bin/which
-fi
-
-# parameters
-
-# The system doing the building
-if [ "x${BUILD}" = "x" ]; then
-    BUILD=$(cc -dumpmachine)
-fi
-
-# Get the host build if unset
-if [ "x${HOST}" = "x" ]; then
-    if [ "x${TARGET_ABI}" = "x" ]; then
-        HOST=${BUILD}
-    else
-        HOST=${TARGET_ABI}
-    fi
-else
-    HOST_CC_LIST="${HOST}-cc ${HOST}-gcc /opt/netsurf/${HOST}/cross/bin/${HOST}-cc /opt/netsurf/${HOST}/cross/bin/${HOST}-gcc"
-    for HOST_CC_V in $(echo ${HOST_CC_LIST});do
-        HOST_CC=$(${WHICH_CMD} ${HOST_CC_V})
-        if [ "x${HOST_CC}" != "x" ];then
-            break
-        fi
-    done
-    if [ "x${HOST_CC}" = "x" ];then
-        echo "Unable to execute host compiler for HOST=${HOST}. is it set correctly?"
-        return 1
-    fi
-
-    HOST_CC_MACHINE=$(${HOST_CC} -dumpmachine 2>/dev/null)
-
-    if [ "${HOST_CC_MACHINE}" != "${HOST}" ];then
-        echo "Compiler dumpmachine differes from HOST setting"
-        return 2
-    fi
-    unset HOST_CC_LIST HOST_CC_V HOST_CC HOST_CC_MACHINE
-fi
-
-if [ "x${TARGET_WORKSPACE}" = "x" ]; then
-    TARGET_WORKSPACE=${HOME}/dev-netsurf/workspace
-fi
-
-if [ "x${USE_CPUS}" = "x" ]; then
-    NCPUS=$(getconf _NPROCESSORS_ONLN 2>/dev/null || getconf NPROCESSORS_ONLN 2>/dev/null)
-    NCPUS="${NCPUS:-1}"
-    NCPUS=$((NCPUS * 2))
-    USE_CPUS="-j${NCPUS}"
-fi
-
-# The GTK version to build for (either 2 or 3 currently)
-if [ "x${NETSURF_GTK_MAJOR}" = "x" ]; then
-    NETSURF_GTK_MAJOR=2
-fi
-
+# Usage: source env.sh
+#
+# Controlling variables
+#   HOST sets the target architecture for library builds
+#   BUILD sets the building machines architecture
+#   TARGET_WORKSPACE is the workspace directory to keep the sandboxes
+#
+# The use of HOST and BUILD here is directly comprable to the GCC
+#   usage as described at:
+#     http://gcc.gnu.org/onlinedocs/gccint/Configure-Terms.html
+#
 
 ###############################################################################
-# Setup environment
+# OS Package installation
 ###############################################################################
-
-echo "BUILD=${BUILD}"
-echo "HOST=${HOST}"
-echo "TARGET_WORKSPACE=${TARGET_WORKSPACE}"
-echo "USE_CPUS=${USE_CPUS}"
-
-export PREFIX=${TARGET_WORKSPACE}/inst-${HOST}
-export PKG_CONFIG_PATH=${PREFIX}/lib/pkgconfig:${PKG_CONFIG_PATH}::
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${PREFIX}/lib
-export PATH=${PATH}:${PREFIX}/bin
-export NETSURF_GTK_MAJOR
-
-# make tool
-MAKE=make
-
-# NetSurf GIT repositories
-NS_GIT="git://git.netsurf-browser.org"
-
-# Buildsystem: everything depends on this
-NS_BUILDSYSTEM="buildsystem"
-
-# internal libraries all frontends require (order is important)
-NS_INTERNAL_LIBS="libwapcaplet libparserutils libhubbub libdom libcss libnsgif libnsbmp libutf8proc libnsutils libnspsl libnslog"
-
-# The browser itself
-NS_BROWSER="netsurf"
-
-
-# add target specific libraries
-case "${HOST}" in
-    i586-pc-haiku)
-        # tools required to build the browser for haiku (beos)
-        NS_TOOLS="nsgenbind"
-        # libraries required for the haiku target abi
-        NS_FRONTEND_LIBS="libsvgtiny"
-        ;;
-    *arwin*)
-        # tools required to build the browser for OS X
-        NS_TOOLS=""
-        # libraries required for the Darwin target abi
-        NS_FRONTEND_LIBS="libsvgtiny libnsfb"
-        ;;
-    arm-unknown-riscos)
-        # tools required to build the browser for RISC OS
-        NS_TOOLS="nsgenbind"
-        # libraries required for the risc os target abi
-        NS_FRONTEND_LIBS="libsvgtiny librufl libpencil librosprite"
-        ;;
-    *-atari-mint)
-        # tools required to build the browser for atari
-        NS_TOOLS=""
-        # libraries required for the atari frontend
-        NS_FRONTEND_LIBS=""
-        ;;
-    ppc-amigaos)
-        # default tools required to build the browser
-        NS_TOOLS="nsgenbind"
-        # default additional internal libraries
-        NS_FRONTEND_LIBS="libsvgtiny"
-        ;;
-    m68k-unknown-amigaos)
-        # default tools required to build the browser
-        NS_TOOLS="nsgenbind"
-        # default additional internal libraries
-        NS_FRONTEND_LIBS="libsvgtiny"
-        ;;
-    *-unknown-freebsd*)
-        # tools required to build the browser for freebsd
-        NS_TOOLS=""
-        # libraries required for the freebsd frontend
-        NS_FRONTEND_LIBS=""
-        # select gnu make
-        MAKE=gmake
-        ;;
-    *)
-        # default tools required to build the browser
-        NS_TOOLS="nsgenbind"
-        # default additional internal libraries
-        NS_FRONTEND_LIBS="libsvgtiny libnsfb"
-        ;;
-esac
-
-export MAKE
-
-################ OS Package installation ################
 
 # deb packages for dpkg based systems
 NS_DEV_DEB="build-essential pkg-config git gperf libcurl3-dev libssl-dev libpng-dev libjpeg-dev"
@@ -293,6 +156,162 @@ ns-package-install()
     fi
 }
 
+###############################################################################
+# Setup environment
+###############################################################################
+
+# find which command used to find everything else on path
+if [ -x /usr/bin/which ]; then
+    WHICH_CMD=/usr/bin/which
+else
+    WHICH_CMD=/bin/which
+fi
+
+# environment parameters
+
+# The system doing the building
+if [ "x${BUILD}" = "x" ]; then
+    BUILD_CC=$(${WHICH_CMD} cc)
+    if [ $? -eq 0 ];then
+        BUILD=$(cc -dumpmachine)
+    else
+       echo "Unable to locate a compiler. Perhaps run ns-package-install"
+       return 1
+    fi
+fi
+
+# Get the host build if unset
+if [ "x${HOST}" = "x" ]; then
+    if [ "x${TARGET_ABI}" = "x" ]; then
+        HOST=${BUILD}
+    else
+        HOST=${TARGET_ABI}
+    fi
+else
+    HOST_CC_LIST="${HOST}-cc ${HOST}-gcc /opt/netsurf/${HOST}/cross/bin/${HOST}-cc /opt/netsurf/${HOST}/cross/bin/${HOST}-gcc"
+    for HOST_CC_V in $(echo ${HOST_CC_LIST});do
+        HOST_CC=$(${WHICH_CMD} ${HOST_CC_V})
+        if [ "x${HOST_CC}" != "x" ];then
+            break
+        fi
+    done
+    if [ "x${HOST_CC}" = "x" ];then
+        echo "Unable to execute host compiler for HOST=${HOST}. is it set correctly?"
+        return 1
+    fi
+
+    HOST_CC_MACHINE=$(${HOST_CC} -dumpmachine 2>/dev/null)
+
+    if [ "${HOST_CC_MACHINE}" != "${HOST}" ];then
+        echo "Compiler dumpmachine differes from HOST setting"
+        return 2
+    fi
+    unset HOST_CC_LIST HOST_CC_V HOST_CC HOST_CC_MACHINE
+fi
+
+# set up a default target workspace
+if [ "x${TARGET_WORKSPACE}" = "x" ]; then
+    TARGET_WORKSPACE=${HOME}/dev-netsurf/workspace
+fi
+
+# set up default parallelism
+if [ "x${USE_CPUS}" = "x" ]; then
+    NCPUS=$(getconf _NPROCESSORS_ONLN 2>/dev/null || getconf NPROCESSORS_ONLN 2>/dev/null)
+    NCPUS="${NCPUS:-1}"
+    NCPUS=$((NCPUS * 2))
+    USE_CPUS="-j${NCPUS}"
+fi
+
+# The GTK version to build for (either 2 or 3 currently)
+if [ "x${NETSURF_GTK_MAJOR}" = "x" ]; then
+    NETSURF_GTK_MAJOR=2
+fi
+
+# report to user
+echo "BUILD=${BUILD}"
+echo "HOST=${HOST}"
+echo "TARGET_WORKSPACE=${TARGET_WORKSPACE}"
+echo "USE_CPUS=${USE_CPUS}"
+
+export PREFIX=${TARGET_WORKSPACE}/inst-${HOST}
+export BUILD_PREFIX=${TARGET_WORKSPACE}/inst-${BUILD}
+export PKG_CONFIG_PATH=${PREFIX}/lib/pkgconfig:${PKG_CONFIG_PATH}::
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${PREFIX}/lib
+export PATH=${PATH}:${BUILD_PREFIX}/bin
+export NETSURF_GTK_MAJOR
+
+# make tool
+MAKE=make
+
+# NetSurf GIT repositories
+NS_GIT="git://git.netsurf-browser.org"
+
+# Buildsystem: everything depends on this
+NS_BUILDSYSTEM="buildsystem"
+
+# internal libraries all frontends require (order is important)
+NS_INTERNAL_LIBS="libwapcaplet libparserutils libhubbub libdom libcss libnsgif libnsbmp libutf8proc libnsutils libnspsl libnslog"
+
+# The browser itself
+NS_BROWSER="netsurf"
+
+
+# add target specific libraries
+case "${HOST}" in
+    i586-pc-haiku)
+        # tools required to build the browser for haiku (beos)
+        NS_TOOLS="nsgenbind"
+        # libraries required for the haiku target abi
+        NS_FRONTEND_LIBS="libsvgtiny"
+        ;;
+    *arwin*)
+        # tools required to build the browser for OS X
+        NS_TOOLS=""
+        # libraries required for the Darwin target abi
+        NS_FRONTEND_LIBS="libsvgtiny libnsfb"
+        ;;
+    arm-unknown-riscos)
+        # tools required to build the browser for RISC OS
+        NS_TOOLS="nsgenbind"
+        # libraries required for the risc os target abi
+        NS_FRONTEND_LIBS="libsvgtiny librufl libpencil librosprite"
+        ;;
+    *-atari-mint)
+        # tools required to build the browser for atari
+        NS_TOOLS=""
+        # libraries required for the atari frontend
+        NS_FRONTEND_LIBS=""
+        ;;
+    ppc-amigaos)
+        # default tools required to build the browser
+        NS_TOOLS="nsgenbind"
+        # default additional internal libraries
+        NS_FRONTEND_LIBS="libsvgtiny"
+        ;;
+    m68k-unknown-amigaos)
+        # default tools required to build the browser
+        NS_TOOLS="nsgenbind"
+        # default additional internal libraries
+        NS_FRONTEND_LIBS="libsvgtiny"
+        ;;
+    *-unknown-freebsd*)
+        # tools required to build the browser for freebsd
+        NS_TOOLS=""
+        # libraries required for the freebsd frontend
+        NS_FRONTEND_LIBS=""
+        # select gnu make
+        MAKE=gmake
+        ;;
+    *)
+        # default tools required to build the browser
+        NS_TOOLS="nsgenbind"
+        # default additional internal libraries
+        NS_FRONTEND_LIBS="libsvgtiny libnsfb"
+        ;;
+esac
+
+export MAKE
+
 ################ Development helpers ################
 
 # git pull in all repos parameters are passed to git pull
@@ -330,17 +349,21 @@ ns-clone()
 # issues a make command to all libraries
 ns-make-libs()
 {
-    for REPO in $(echo ${NS_BUILDSYSTEM} ${NS_TOOLS}); do
+    for REPO in $(echo ${NS_BUILDSYSTEM} ${NS_INTERNAL_LIBS} ${NS_FRONTEND_LIBS}); do
         echo "    MAKE: make -C ${REPO} $USE_CPUS $*"
-        ${MAKE} -C ${TARGET_WORKSPACE}/${REPO} $USE_CPUS $*
+        ${MAKE} -C ${TARGET_WORKSPACE}/${REPO} HOST=${HOST} $USE_CPUS $*
         if [ $? -ne 0 ]; then
             return $?
         fi
     done
+}
 
-    for REPO in $(echo ${NS_INTERNAL_LIBS} ${NS_FRONTEND_LIBS}); do
+# issues make command for all tools
+ns-make-tools()
+{
+    for REPO in $(echo ${NS_BUILDSYSTEM} ${NS_TOOLS}); do
         echo "    MAKE: make -C ${REPO} $USE_CPUS $*"
-        ${MAKE} -C ${TARGET_WORKSPACE}/${REPO} HOST=${HOST} $USE_CPUS $*
+        ${MAKE} -C ${TARGET_WORKSPACE}/${REPO} PREFIX=${BUILD_PREFIX} HOST=${BUILD} $USE_CPUS $*
         if [ $? -ne 0 ]; then
             return $?
         fi
@@ -359,6 +382,7 @@ ns-pull-install()
 {
     ns-pull $*
 
+    ns-make-tools install
     ns-make-libs install
 }
 
