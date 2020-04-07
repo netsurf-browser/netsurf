bdd1d2039cd963354f99c4bb4a7270848bda1081
Michael Drake <michael.drake@codethink.co.uk>
2020-04-07 20:55:37 +0100

    nsoption: Improve how we show colours in about:config.

--- utils/nsoption.c
+++ utils/nsoption.c
@@ -386,12 +386,17 @@ nsoption_output_value_html(struct nsoption_s *option,
 		rgbcolour = colour_rb_swap(option->value.c);
 		slen = snprintf(string + pos,
 				size - pos,
+				"<span style=\"font-family:Monospace;\">"
+					"#%06X"
+				"</span> "
 				"<span style=\"background-color: #%06x; "
-				"color: #%06x; "
-				"font-family:Monospace; \">#%06X</span>",
+					"border: 1px solid #%06x; "
+					"display: inline-block; "
+					"width: 1em; height: 1em;\">"
+				"</span>",
 				rgbcolour,
-				colour_to_bw_furthest(rgbcolour),
-				rgbcolour);
+				rgbcolour,
+				colour_to_bw_furthest(rgbcolour));
 		break;
 
 	case OPTION_STRING:
