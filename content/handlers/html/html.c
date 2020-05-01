19cd5cb3b3771592110b7400558727b71a005f63
Vincent Sanders <vince@kyllikki.org>
2020-05-01 21:36:50 +0100

    add missing include

--- content/handlers/html/html.c
+++ content/handlers/html/html.c
@@ -56,8 +56,9 @@
 #include "desktop/gui_internal.h"
 
 #include "html/html.h"
-#include "html/html_save.h"
 #include "html/html_internal.h"
+#include "html/object.h"
+#include "html/html_save.h"
 #include "html/interaction.h"
 #include "html/box.h"
 #include "html/box_construct.h"
