7f2702294f83dc81080fc651272749a4ccd62daf
Vincent Sanders <vince@kyllikki.org>
2020-05-24 21:55:29 +0100

    Update version files for release
    Signed-off-by: Vincent Sanders <vince@kyllikki.org>

--- desktop/version.c
+++ desktop/version.c
@@ -20,11 +20,6 @@
 
 #include "desktop/version.h"
 
-const char * const netsurf_version = "3.10 (Dev"
-#if defined(CI_BUILD)
-	" CI #" CI_BUILD
-#endif
-	")"
-	;
+const char * const netsurf_version = "3.10 (24th May 2020)";
 const int netsurf_version_major = 3;
 const int netsurf_version_minor = 10;
