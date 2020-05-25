9071b450be74d136c48cd97d8205593532056ca6
Vincent Sanders <vince@kyllikki.org>
2020-05-25 08:57:09 +0100

    update jenkins build script after 3.10 release

--- utils/jenkins-build.sh
+++ utils/jenkins-build.sh
@@ -51,6 +51,9 @@ ATARIARCH=68020-60
 # make tool
 MAKE=make
 
+# NetSurf version number haiku needs it for package name
+NETSURF_VERSION="3.11"
+
 # Ensure the combination of target and toolchain works and set build
 #   specific parameters too
 case ${TARGET} in
@@ -82,7 +85,7 @@ case ${TARGET} in
 
 	esac
 
-	PKG_SRC=netsurf_x86-3.10-1-x86_gcc2
+	PKG_SRC="netsurf_x86-${NETSURF_VERSION}-1-x86_gcc2"
 	PKG_SFX=.hpkg
 	;;
 
