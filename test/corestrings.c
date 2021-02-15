bc15ad5784a0e653cdc07ec9c1dbd2ea2e45f12e
Michael Drake <michael.drake@codethink.co.uk>
2021-02-15 14:24:15 +0000

    tests: Update corestring allocation count.

--- test/corestrings.c
+++ test/corestrings.c
@@ -40,7 +40,7 @@
  *
  * This is used to test all the out of memory paths in initialisation.
  */
-#define CORESTRING_TEST_COUNT 483
+#define CORESTRING_TEST_COUNT 487
 
 START_TEST(corestrings_test)
 {
