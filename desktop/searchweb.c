d62fca3af67418c7f7708461abfb0b18943728c9
Vincent Sanders <vince@kyllikki.org>
2025-02-23 21:56:56 +0000

    work around frontends that do not initialise the web search providers

--- desktop/searchweb.c
+++ desktop/searchweb.c
@@ -351,7 +351,13 @@ search_web_omni(const char *term,
 		}
 	}
 
-	/* must be initialised */
+	/* ensure providers are initialised */
+	if (search_web_ctx.providers == NULL) {
+		ret = search_web_init(NULL);
+		if (ret != NSERROR_OK) {
+			return ret;
+		}
+	}
 	if (search_web_ctx.providers == NULL) {
 		return NSERROR_INIT_FAILED;
 	}
