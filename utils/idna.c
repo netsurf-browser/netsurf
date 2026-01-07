504704f656717aebdd8fad96ec97fa977d87ca57
Michael Drake <tlsa@netsurf-browser.org>
2026-01-07 22:31:01 +0000

    env: Use upstream utf8proc

--- utils/idna.c
+++ utils/idna.c
@@ -167,7 +167,7 @@ idna__ace_to_ucs4(const char *ace_label,
 
 #ifdef WITH_UTF8PROC
 
-#include <libutf8proc/utf8proc.h>
+#include <utf8proc.h>
 
 int32_t idna_contexto[] = {
 	/* CONTEXTO codepoints which have a rule defined */
